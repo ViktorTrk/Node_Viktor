58fdd16b8e7c92e10f3c85ddf9856d032d5e13d7
Viktor T <viktor@pacto.io>
2017-01-12 19:12:14 +0200

    global obj timeout

---
+++ main16_glob.js
@@ -0,0 +1,54 @@
+// console.log(__filename);
+// console.log(__dirname);	
+
+	// Timeout
+
+// 	function printHello(){
+//    console.log( "FIVE!");
+// }
+// // Now call above function after 5 seconds
+// setTimeout(printHello, 5000);
+
+// function print1(){
+// 	console.log("1");
+// }
+// setTimeout(print1, 1000);
+
+// function print2(){
+// 	console.log("2");
+// }
+// setTimeout(print2, 2000);
+
+// function print3(){
+// 	console.log("3");
+// }
+// setTimeout(print3, 3000);
+
+// function print4(){
+// 	console.log("4");
+// }
+// setTimeout(print4, 4000);
+
+
+	//clearTimeout(t)
+
+// function printHello(){
+//    console.log( "Hello, World!");
+// }
+
+// // Now call above function after 2 seconds
+// var t = setTimeout(printHello, 2000);
+
+// // Now clear the timer
+// clearTimeout(t);
+
+	// setInterval
+
+
+function printHello(){
+   console.log( "Hello, World!");
+}
+// Now call above function after 2 seconds
+setInterval(printHello, 500);
+
+
