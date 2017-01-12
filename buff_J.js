b174e29e6ece10887187399c99bfbcc80d9febe8
Viktor T <viktor@pacto.io>
2017-01-12 16:14:57 +0200

    buffer2

---
+++ buff_J.js
@@ -0,0 +1,25 @@
+// var buf = new Buffer('Simply Easy Learning');
+// var json = buf.toJSON(buf);
+
+// console.log(json);
+
+// var buffer1 = new Buffer('ABC');
+// var buffer2 = new Buffer('ABCD');
+// var result = buffer1.compare(buffer2);
+
+// var buffer1 = new Buffer('ABC');
+// var buffer2 = new Buffer('ABC');
+// var result = buffer1.compare(buffer2);
+
+// if(result < 0) {
+//    console.log(buffer1 +" comes before " + buffer2);
+// }else if(result == 0){
+//    console.log(buffer1 +" is same as " + buffer2);
+// }else {
+//    console.log(buffer1 +" comes after " + buffer2);
+// }
+
+
+var buffer = new Buffer('TutorialsPoint');
+//length of the buffer
+console.log("buffer length: " + buffer.length);
