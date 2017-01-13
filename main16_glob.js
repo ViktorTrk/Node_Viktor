6192240268161f5a7c01032558d953540d06ba47
Viktor T <viktor@pacto.io>
2017-01-13 12:19:51 +0200

    express

--- main16_glob.js
+++ main16_glob.js
@@ -3,31 +3,31 @@
 
 	// Timeout
 
-// 	function printHello(){
-//    console.log( "FIVE!");
-// }
-// // Now call above function after 5 seconds
-// setTimeout(printHello, 5000);
+	function printHello(){
+   console.log( "FIVE!");
+}
+// Now call above function after 5 seconds
+setTimeout(printHello, 5000);
 
-// function print1(){
-// 	console.log("1");
-// }
-// setTimeout(print1, 1000);
+function print1(){
+	console.log("1");
+}
+setTimeout(print1, 1000);
 
-// function print2(){
-// 	console.log("2");
-// }
-// setTimeout(print2, 2000);
+function print2(){
+	console.log("2");
+}
+setTimeout(print2, 2000);
 
-// function print3(){
-// 	console.log("3");
-// }
-// setTimeout(print3, 3000);
+function print3(){
+	console.log("3");
+}
+setTimeout(print3, 3000);
 
-// function print4(){
-// 	console.log("4");
-// }
-// setTimeout(print4, 4000);
+function print4(){
+	console.log("4");
+}
+setTimeout(print4, 4000);
 
 
 	//clearTimeout(t)
@@ -45,10 +45,10 @@
 	// setInterval
 
 
-function printHello(){
-   console.log( "Hello, World!");
-}
-// Now call above function after 2 seconds
-setInterval(printHello, 500);
+// function printHello(){
+//    console.log( "Hello, World!");
+// }
+// // Now call above function after 2 seconds
+// setInterval(printHello, 500);
 
 
