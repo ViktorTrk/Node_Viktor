8f4055c792d37956ce05bb0dbf752ba89ce9295c
Viktor T <viktor@pacto.io>
2017-01-13 16:37:51 +0200

    Express frame

--- serv_ex.js
+++ serv_ex.js
@@ -114,29 +114,33 @@ var fs = require("fs");
 
 var bodyParser = require('body-parser');
 var multer  = require('multer');
+var upload = multer({ dest: '/tmp/'});
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}));
+//app.use(multer({ dest: '/tmp/'}));
+
 
 app.get('/index_get.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index_get.htm" );
 })
 
-app.post('/file_upload', function (req, res) {
-   console.log(req.files.file.name);
-   console.log(req.files.file.path);
-   console.log(req.files.file.type);
-   var file = __dirname + "/" + req.files.file.name;
+app.post('/file_upload', upload.single('file'), function (req, res) {
+   console.log(req.file.name);
+   console.log(req.file.path);
+   console.log(req.file.type);
+   var file = __dirname + "/" + req.file.originalname;
    
-   fs.readFile( req.files.file.path, function (err, data) {
+   console.log("writing file called: ", file);
+
+   fs.readFile( req.file.path, function (err, data) {
       fs.writeFile(file, data, function (err) {
          if( err ){
-            console.log( err );
+            console.log("error", err );
             }else{
                response = {
                   message:'File uploaded successfully',
-                  filename:req.files.file.name
+                  filename: file
                };
             }
          console.log( response )
@@ -151,3 +155,50 @@ var server = app.listen(8081, function () {
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
+
+
+// WRONG upload:
+
+// var express = require('express');
+// var app = express();
+// var fs = require("fs");
+
+// var bodyParser = require('body-parser');
+// var multer  = require('multer');
+
+// app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(multer({ dest: '/tmp/'}));
+
+// app.get('/index.htm', function (req, res) {
+//    res.sendFile( __dirname + "/" + "index.htm" );
+// })
+
+// app.post('/file_upload', function (req, res) {
+//    console.log(req.files.file.name);
+//    console.log(req.files.file.path);
+//    console.log(req.files.file.type);
+//    var file = __dirname + "/" + req.files.file.name;
+   
+//    fs.readFile( req.files.file.path, function (err, data) {
+//       fs.writeFile(file, data, function (err) {
+//          if( err ){
+//             console.log( err );
+//             }else{
+//                response = {
+//                   message:'File uploaded successfully',
+//                   filename:req.files.file.name
+//                };
+//             }
+//          console.log( response );
+//          res.end( JSON.stringify( response ) );
+//       });
+//    });
+// })
+
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+   
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
